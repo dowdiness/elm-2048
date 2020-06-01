6bd02a7805e0d66da92f80eab6a2731bafc3392e
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-06-01 22:23:05 +0900

    setup basic grid

--- src/Main.elm
+++ src/Main.elm
@@ -2,20 +2,14 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Nav
-import Data
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (attribute, class, disabled, href)
 import Html.Styled.Events exposing (onClick)
-import Http
 import RemoteData exposing (RemoteData)
-import Route exposing (Route(..))
 import Url exposing (Url)
-import View exposing (navIn, navOut)
-
 
 type alias Flags =
     {}
-
 
 
 -- MODEL
@@ -23,18 +17,12 @@ type alias Flags =
 
 type alias Model =
     { key : Nav.Key
-    , route : Route.Route
-    , package : RemoteData Http.Error Data.Package
+    , route : Url.Url
+    , tiles: List Int
     }
 
-
-type Msg
-    = -- Message naming conventions: https://youtu.be/w6OVDBqergc
-    BrowserChangedUrl Url
-    | UserClickedLink Browser.UrlRequest
-    | UserClickedPackageButton
-    | ServerRespondedWithPackage (Result Http.Error Data.Package)
-
+initialTiles =
+    List.repeat 16 2048
 
 main : Program Flags Model Msg
 main =
@@ -51,151 +39,49 @@ main =
 
 -- VIEW
 
-
-project : { description : String, title : String, url : String }
-project =
-    { title = "Elm Batteries Included!"
-    , description = """
-        Develop Elm apps with Parcel, Netlify, Tailwind CSS and Cypress.
-        Learn how these delightful web technologies work together.
-        Get started with Elm navigation, routes, remote data and JSON decoder
-    """
-    , url = "https://github.com/cedricss/elm-batteries#elm-batteries-included"
-    }
-
-
 view : Model -> Browser.Document Msg
 view model =
-    { title = project.title
-    , body = [ body model |> div [] |> toUnstyled ]
+    { title = "Elm 2048!"
+    , body = [ body model |> toUnstyled ]
     }
 
-
-body : Model -> List (Html Msg)
+body : Model -> Html Msg
 body model =
-    [ View.header
-        [ navIn "Home" "/"
-        , navIn "Demo" "/demo"
-        , navOut "Documentation" "https://concat.dev/elm"
-        , navOut "Twitter" "https://twitter.com/CedricSoulas"
-        , navOut "Github" "https://github.com/cedricss/elm-batteries"
+    div
+        []
+        [ h1
+            [ class "text-gray-800" ]
+            [ text "Elm 2048" ]
+        , gameBoard model
         ]
-    , View.container <|
-        case model.route of
-            ApiDemo ->
-                viewDemo model
 
-            Home ->
-                viewHome model
+gameBoard : Model -> Html Msg
+gameBoard model =
+    div
+        [ class "w-64 sm:w-96 flex flex-wrap"]
+        (List.map gameTile (List.map String.fromInt model.tiles))
 
-            NotFound ->
-                View.notFound
-    ]
-
-
-viewHome : Model -> List (Html Msg)
-viewHome model =
-    [ h1 [] [ text "Elm Batteries Included" ]
-    , p
-        [ class "max-w-2xl text-xl mb-4" ]
-        [ text project.description ]
-    , ul
-        [ class "text-xl" ]
-        [ li [] [ a [ href "/demo" ] [ text "Try the demo ›" ] ]
-        , li [] [ a [ href project.url ] [ text "Read the documentation ›" ] ]
-        ]
-    ]
-
-
-viewDemo : Model -> List (Html Msg)
-viewDemo model =
-    let
-        content attributes =
-            ul <|
-                [ attribute "data-test" "package"
-                , class "mt-8 flex flex-col justify-center max-w-2xl h-48"
-                , class "bg-transition rounded"
-                , class "pl-10 font-semibold leading-loose"
-                ]
-                    ++ attributes
-
-        item key value =
-            li [] (View.keyValue key value)
-
-        fetchButton =
-            button
-                [ class "w-56"
-                , attribute "data-action" "fetch-package"
-                , onClick UserClickedPackageButton
-                ]
-                [ text "Fetch package.json" ]
-    in
-    [ h1 [] [ text "Demo" ]
-    , h2 [] [ text "Serverless Lambda function on Netlify" ]
-    , p
-        [ class "max-w-xl text-xl mb-8" ]
-        [ text "The demo function has a faux 500ms delay to simulate a slower connection and illustrate the loading state. "
-        , a
-            [ href "https://github.com/cedricss/elm-batteries/blob/master/functions/demo/demo.js#L5" ]
-            [ text "Learn more ›" ]
-        ]
-    , div [] <|
-        case model.package of
-            RemoteData.Success p ->
-                [ fetchButton
-                , content
-                    [ attribute "data-result" "success"
-                    , class "bg-gray-300"
-                    ]
-                    [ item "name" p.name
-                    , item "url" p.url
-                    , item "author" p.author
-                    , item "license" p.license
-                    ]
-                ]
-
-            RemoteData.NotAsked ->
-                [ fetchButton
-                , content
-                    [ attribute "data-result" "not-asked"
-                    , class "bg-white"
-                    ]
-                    []
-                ]
-
-            RemoteData.Loading ->
-                [ button
-                    [ class "w-56 cursor-wait"
-                    , disabled True
-                    ]
-                    [ text "Loading..." ]
-                , content
-                    [ attribute "data-result" "loading"
-                    , class "bg-gray-500"
-                    ]
-                    []
-                ]
-
-            RemoteData.Failure _ ->
-                [ fetchButton
-                , content
-                    [ attribute "data-result" "error"
-                    , class "bg-red-500 p-12 text-white text-center"
-                    ]
-                    [ li [] [ text "Oops! Something went wrong..." ] ]
-                ]
-    ]
-
+gameTile : String -> Html Msg
+gameTile number =
+    div
+        [ class "w-16 h-16 sm:w-24 sm:h-24 border flex items-center justify-center" ]
+        [ p [ class "text-2xl font-bold"] [ text number ] ]
 
 
 -- UPDATE
+
+
+type Msg
+    = -- Message naming conventions: https://youtu.be/w6OVDBqergc
+    BrowserChangedUrl Url
+    | UserClickedLink Browser.UrlRequest
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         BrowserChangedUrl url ->
-            ( { model | route = Route.fromUrl url }
+            ( { model | route = url }
             , Cmd.none
             )
 
@@ -206,42 +92,17 @@ update msg model =
                     , Nav.pushUrl model.key (Url.toString url)
                     )
 
-                Browser.External url ->
+                Browser.External href ->
                     ( model
-                    , Nav.load url
+                    , Nav.load href
                     )
-
-        UserClickedPackageButton ->
-            ( { model | package = RemoteData.Loading }
-            , Http.get
-                { url = "/.netlify/functions/demo"
-                , expect =
-                    Http.expectJson
-                        ServerRespondedWithPackage
-                        Data.packageDecoder
-                }
-            )
-
-        ServerRespondedWithPackage result ->
-            ( { model | package = RemoteData.fromResult result }
-            , Cmd.none
-            )
 
 
 init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    let
-        route =
-            Route.fromUrl url
-    in
-    ( { key = key
-      , route = route
-      , package = RemoteData.NotAsked
-      }
-    , Cmd.none
-    )
+init _ url key =
+    ( { key = key, route = url, tiles = initialTiles }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        sm: "480px"
+      },
+      width: {
+        96: "24rem",
+        128: "32rem"
+      }
+    }
   },
   variants: {},
   plugins: []
