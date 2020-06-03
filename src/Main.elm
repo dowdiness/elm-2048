e3ac8f6aefd87eae533a47523f5d7b7e0f56fd5d
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-06-04 00:10:48 +0900

    add random tile generation

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,16 @@ module Main exposing (main)
 import Browser
 import Browser.Navigation as Nav
 import Browser.Events exposing (onKeyDown)
-import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (attribute, class, disabled, href)
-import Html.Styled.Events exposing (onClick)
-import RemoteData exposing (RemoteData)
+import Element exposing (Element, el, text, paragraph, row, column, alignRight, fill, width, height, px, rgb255, spacing, centerX, centerY, padding)
+import Element.Input as Input
+import Element.Events exposing (onClick)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 import Url exposing (Url)
 import Json.Decode as Decode
+import Random
+import Array
 
 ---- PROGRAM ----
 
@@ -24,22 +28,45 @@ main =
         }
 
 
-
 -- MODEL
 
 
 type alias Model =
     { key : Nav.Key
     , route : Url.Url
-    , tiles: List Int
-    , pressed: String
+    , board : Board
     }
+
+type alias Board =
+    List Row
+
+type alias Row =
+    List Cell
+
+type Cell
+    = Tile Int
+    | Empty
+
+type alias Position =
+    (Int, Int)
 
 type alias Flags =
     {}
 
-initialTiles =
-    List.repeat 16 2048
+emptyBoard : Board
+emptyBoard = List.repeat 4 <| List.repeat 4 <| Empty
+
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { key = key, route = url, board = emptyBoard }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ onKeyDown directionDecoderToEventMoveTileMsg
+        ]
+
 
 -- Decoder
 
@@ -48,61 +75,91 @@ type Direction
     | Right
     | Down
     | Left
-    | Other String
+directionDecoderToEventMoveTileMsg : Decode.Decoder Msg
+directionDecoderToEventMoveTileMsg =
+    Decode.map EventMoveTile directionDecoder
 
-keyDecoder : Decode.Decoder Direction
+directionDecoder : Decode.Decoder (Maybe Direction)
+directionDecoder =
+    keyDecoder
+        |> Decode.andThen chooseFromDirection
+
+keyDecoder : Decode.Decoder String
 keyDecoder =
-    Decode.map toDirection (Decode.field "key" Decode.string)
+    Decode.field "key" Decode.string
 
-toDirection : String -> Direction
-toDirection string =
+chooseFromDirection : String -> Decode.Decoder (Maybe Direction)
+chooseFromDirection string =
     case string of
         "ArrowUp" ->
-            Up
+            Decode.succeed (Just Up)
 
         "ArrowRight" ->
-            Right
+            Decode.succeed (Just Right)
 
         "ArrowDown" ->
-            Down
+            Decode.succeed (Just Down)
 
         "ArrowLeft" ->
-            Left
+            Decode.succeed (Just Left)
 
         _ ->
-            Other string
-
+            Decode.succeed Nothing
 
 -- VIEW
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Elm 2048!"
-    , body = [ body model |> toUnstyled ]
+    , body = [ body model |> Element.layout [] ]
     }
 
-body : Model -> Html Msg
+
+body : Model -> Element Msg
 body model =
-    div
-        []
-        [ h1
-            [ class "text-gray-800" ]
-            [ text "Elm 2048" ]
-        , h2 [] [ text model.pressed ]
-        , gameBoard model
+    column
+        [centerX, centerY, spacing 12]
+        [ el [Font.size 42] <| text "Elm 2048"
+        , el [Background.color (rgb255 178 245 234), padding 16] (gameBoard model.board)
+        , Input.button
+            [ Font.color (rgb255 255 255 255), padding 8, Background.color (rgb255 117 118 119), alignRight ]
+            { onPress = Just StartGame
+            , label = text "Start"
+            }
         ]
 
-gameBoard : Model -> Html Msg
-gameBoard model =
-    div
-        [ class "w-64 flex flex-wrap"]
-        (List.map gameTile (List.map String.fromInt model.tiles))
 
-gameTile : String -> Html Msg
-gameTile number =
-    div
-        [ class "w-16 h-16 border flex items-center justify-center" ]
-        [ p [ class "text-2xl font-bold"] [ text number ] ]
+gameBoard : Board -> Element msg
+gameBoard board =
+    column
+        [ spacing 16 ]
+        (List.map gameRow board)
+
+gameRow : Row -> Element msg
+gameRow model =
+    row
+        [ spacing 16 ]
+        (List.map gameTile model)
+
+gameTile : Cell -> Element msg
+gameTile cell =
+    case cell of
+        Tile number ->
+            column
+                [ width <| px 64, height <| px 64
+                , Background.color <| rgb255 79 209 197
+                ]
+                [ el [Font.size 32, centerX, centerY]
+                    <| text
+                    <| String.fromInt number ]
+        Empty ->
+            el
+                [ width <| px 64, height <| px 64
+                , Background.color (rgb255 79 209 197)
+                , centerX
+                , centerY
+                ]
+                Element.none
 
 
 -- UPDATE
@@ -112,7 +169,9 @@ type Msg
     = -- Message naming conventions: https://youtu.be/w6OVDBqergc
     BrowserChangedUrl Url
     | UserClickedLink Browser.UrlRequest
-    | UserPressedKey Direction
+    | EventMoveTile (Maybe Direction)
+    | StartGame
+    | AddTile (Position, Cell)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -135,41 +194,70 @@ update msg model =
                     , Nav.load href
                     )
 
-        UserPressedKey key ->
-            case key of
-                Up ->
-                    ( { model | pressed = "Up" }
-                    , Cmd.none
+        EventMoveTile maybeDirection ->
+            case maybeDirection of
+                Just direction ->
+                    let
+                        _ = Debug.log "direction" direction
+                    in
+                    ( model, Cmd.none )
+                Nothing ->
+                    ( model, Cmd.none)
+        StartGame ->
+            initBoard model
+        AddTile (position, cell) ->
+            ( { model
+                | board = setBoard position cell model.board  }
+            , Cmd.none
+            )
+
+
+initBoard : Model -> ( Model, Cmd Msg )
+initBoard model =
+    ( model
+    , randomPosition model.board
+        |> Maybe.map (\position -> Random.pair position randomTile)
+        |> Maybe.map (Random.generate AddTile)
+        |> Maybe.withDefault Cmd.none
+    )
+
+setBoard : Position -> Cell -> Board -> Board
+setBoard ( x, y ) cell board =
+    let
+        arrayBoard = Array.fromList board
+    in
+        Array.get x arrayBoard
+            |> Maybe.map (\oldRow -> Array.toList(Array.set y cell (Array.fromList oldRow)))
+            |> Maybe.map (\newRow -> Array.set x newRow arrayBoard)
+            |> Maybe.map (\newBoard -> Array.toList newBoard)
+            |> Maybe.withDefault board
+
+
+emptyPositionList : Board -> List Position
+emptyPositionList board =
+            board
+                |> List.indexedMap (\i -> List.indexedMap (\j -> Tuple.pair ( i, j )))
+                |> List.concat
+                |> List.filterMap
+                    (\( position, cell ) ->
+                        case cell of
+                            Tile _ ->
+                                Nothing
+
+                            Empty ->
+                                Just position
                     )
 
-                Right ->
-                    ( { model | pressed = "Right" }
-                    , Cmd.none
-                    )
+randomPosition : Board -> Maybe (Random.Generator Position)
+randomPosition board =
+    case emptyPositionList board of
+        [] ->
+            Nothing
 
-                Down ->
-                    ( { model | pressed = "Down" }
-                    , Cmd.none
-                    )
-
-                Left ->
-                    ( { model | pressed = "Left" }
-                    , Cmd.none
-                    )
-
-                Other pressedKey ->
-                    ( { model | pressed = pressedKey  }
-                    , Cmd.none
-                    )
+        head :: tail ->
+            Just (Random.uniform head tail)
 
 
-init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url key =
-    ( { key = key, route = url, tiles = initialTiles, pressed = "Start" }, Cmd.none )
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.batch
-        [ onKeyDown (Decode.map UserPressedKey keyDecoder)
-        ]
+randomTile : Random.Generator Cell
+randomTile =
+    Random.uniform (Tile 2) [ Tile 4 ]
